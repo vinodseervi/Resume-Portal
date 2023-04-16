50b5097f0fa26d4d7c3db5811646271f768158d7
= <vinodseervi0124@gmail.com>
2023-04-16 12:22:00 +0530

    Added a profile view and Thymeleaf

--- src/main/java/com/java/resumeportal/controller/DemoController.java
+++ src/main/java/com/java/resumeportal/controller/DemoController.java
@@ -1,6 +1,5 @@
 package com.java.resumeportal.controller;
 
-import jakarta.persistence.GeneratedValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +11,11 @@ public class DemoController {
     @GetMapping("/hello")
     public String hello(){
         return "hello world, this is vinod!!";
+    }
+
+    @GetMapping("/edit")
+    public String edit(){
+        return "edit page";
     }
 
 }
--- src/main/java/com/java/resumeportal/controller/UserRestController.java
+++ src/main/java/com/java/resumeportal/controller/UserRestController.java
@@ -8,12 +8,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class UserRestController {
 
-    @GetMapping("/edit")
-    public String edit(){
-        return "edit page";
-    }
 
-    @GetMapping("/view")
+    @GetMapping("/view/{userId}")
     public String view(@PathVariable String userId, Model model){
         model.addAttribute("userId", userId);
         return "profile";
